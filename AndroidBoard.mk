84440a6e829a6d0ad3dfac91808a969fc5f037e9
Yogendra Charya Tangalapally <yogendra@codeaurora.org>
2017-10-06 17:49:50 +0530

    Changes to pick msm8909-256mb-perf_defconfig by default
    Change-Id: If68530618ab3a0454a9f595e33152c38e9883ddb

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -34,7 +34,7 @@ endif
 # Compile Linux Kernel
 #----------------------------------------------------------------------
 ifeq ($(KERNEL_DEFCONFIG),)
-    KERNEL_DEFCONFIG := msm8909-256mb_defconfig
+    KERNEL_DEFCONFIG := msm8909-256mb-perf_defconfig
 endif
 
 include kernel/AndroidKernel.mk
